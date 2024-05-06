cd2517d047e8bf23bebba8838298df530fc385fe
dvs-codes <132570673+dvs-codes@users.noreply.github.com>
2024-05-06 19:09:19 +0530

    create the move graph with by looping through every cell.

--- script.js
+++ script.js
@@ -1,8 +1,57 @@
 let chessBoard = []
 
-for (let i=0; i<64; i++) {
-    chessBoard[i]=[]
+
+
+
+const graphMaker = function() {
+    for (let i=0; i<64; i++) {
+        chessBoard[i]=[]
+    }
+    chessBoard.forEach((item, index) => {
+        let convertedArray = [index%8, (index-index%8)/8]
+
+        const stepMaker = function (x, y, arrayList) {
+            let nextXCordinate = convertedArray[0]+x
+            let nextYCordinate = convertedArray[1]+y
+    
+            let newIndex = nextXCordinate + nextYCordinate*8
+    
+            item.push(newIndex)
+        }
+        //up right, check for out of board
+        if (convertedArray[0]<=6 && convertedArray[1]<=5) {
+            stepMaker(+1, +2)
+        } 
+        //up left
+        if (convertedArray[0]>=1 && convertedArray[1]<=5) {
+            stepMaker(-1, +2)
+        }
+        //right top
+        if (convertedArray[0]<=5 && convertedArray[1]<=6) {
+            stepMaker(+2, +1)
+        }
+        //right down
+        if (convertedArray[0]<=5 && convertedArray[1]>=1) {
+            stepMaker(+2, -1)
+        }
+        //down right
+        if (convertedArray[0]<=6 && convertedArray[1]>=2) {
+            stepMaker(+1, -2)
+        }
+        //down left
+        if (convertedArray[0]>=1 && convertedArray[1]>=2) {
+            stepMaker(-1, -2)
+        }
+        //left up
+        if (convertedArray[0]>=2 && convertedArray[1]<=6) {
+            stepMaker(-2, +1)
+        }//left down
+        if (convertedArray[0]>=2 && convertedArray[1]>=1) {
+            stepMaker(-2, -1)
+        }
+    })
 }
+graphMaker()
 
 const knightMoves = function (startCell, endCell, queue = [startCell], steps = [startCell[0]+startCell[1]*8], paths = []) {
     //check for out of bound
@@ -30,7 +79,6 @@ const knightMoves = function (startCell, endCell, queue = [startCell], steps = [
                         paths.push(steps)
                     }
                 }
-                chessBoard[currenIndex].push(newIndex)
             }
             
             //up right, check for out of board
@@ -75,6 +123,5 @@ const knightMoves = function (startCell, endCell, queue = [startCell], steps = [
 }
 
 console.log(knightMoves([1,1], [0,7]))
-
 console.log(chessBoard)
 
